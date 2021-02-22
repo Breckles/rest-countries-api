9c4ef29dfb7f9ec12c20dd07df1a16fe2da96394
Waylon Carrier Nunes <wayloncn@gmail.com>
2021-02-22 16:19:26 -0500

    - created header component - styled header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './pages/home/home.page';
 import { CountryCardComponent } from './components/country-card/country-card.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, HomePageComponent, CountryCardComponent],
+  declarations: [AppComponent, HomePageComponent, CountryCardComponent, HeaderComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
