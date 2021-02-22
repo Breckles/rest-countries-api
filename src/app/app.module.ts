b3f047b3c30c429baf37023906c455546a883661
Waylon Carrier Nunes <wayloncn@gmail.com>
2021-02-22 17:25:38 -0500

    - created search bar component - styled the search bar component - added the search bar component to the home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './pages/home/home.page';
 import { CountryCardComponent } from './components/country-card/country-card.component';
 import { HeaderComponent } from './components/header/header.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
 
 @NgModule({
-  declarations: [AppComponent, HomePageComponent, CountryCardComponent, HeaderComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    HomePageComponent,
+    CountryCardComponent,
+    HeaderComponent,
+    SearchBarComponent,
+  ],
+  imports: [BrowserModule, ReactiveFormsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/components/search-bar/search-bar.component.ts
@@ -0,0 +1,24 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-search-bar',
+  templateUrl: './search-bar.component.html',
+  styleUrls: ['./search-bar.component.scss'],
+})
+export class SearchBarComponent implements OnInit {
+  @Output()
+  onSearch = new EventEmitter<string>();
+  searchText = new FormControl('', Validators.required);
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onSubmit() {
+    if (this.searchText.valid) {
+      this.onSearch.next(this.searchText.value);
+      this.searchText.reset();
+    }
+  }
+}
